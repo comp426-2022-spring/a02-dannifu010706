f10c230d723ec76b626e8e21cdb8115bcb857444
Nifu Dan <dannifu010706@outlook.com>
2022-03-23 08:54:23 -0400

    a22

--- guess-flip.js
+++ guess-flip.js
@@ -5,8 +5,11 @@ const require = createRequire(import.meta.url);
 var args = require('minimist')(process.argv.slice(2))
 args["call"]
 const call = args.call
-if(call!="heads"&&call!="tails"){
-    console.log("Error: Invalid Input")
+if(call==null){
+    console.log("Error: no input.")
+}
+else if(call!="heads"&&call!="tails"){
+    console.log("Error: no Input.")
     console.log("Usage: node guess-flip --call=[heads|tails]")
 }
 else{
